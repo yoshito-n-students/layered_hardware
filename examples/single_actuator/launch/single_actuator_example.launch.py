2f1f6965a56e189c96de6cdd41e2ac424cb086fb
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2024-10-18 06:31:51 +0000

    support custom robot description in single actuator example

--- examples/single_actuator/launch/single_actuator_example.launch.py
+++ examples/single_actuator/launch/single_actuator_example.launch.py
@@ -8,35 +8,47 @@ from launch_ros.descriptions import ParameterValue
 from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
+    # Default argument values
+    default_robot_description_path = PathJoinSubstitution(
+        [
+            FindPackageShare("layered_hardware"),
+            "examples/single_actuator/description",
+            "robot_description.urdf.xacro",
+        ]
+    )
+
     # Declare arguments
     declared_arguments = []
-    #declared_arguments.append(
-    #    DeclareLaunchArgument(
-    #        "gui",
-    #        default_value="true",
-    #        description="Start RViz2 automatically with this launch file.",
-    #    )
-    #)
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "robot_description_path",
+            default_value=default_robot_description_path,
+            description="Path to robot URDF/xacro file",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "gui",
+            default_value="true",
+            description="Start RViz2 automatically with this launch file",
+        )
+    )
 
     # Initialize Arguments
-    # gui = LaunchConfiguration("gui")
+    robot_description_path = LaunchConfiguration("robot_description_path")
+    gui = LaunchConfiguration("gui")
 
     # Get URDF via xacro
     robot_description_content = Command(
         [
             PathJoinSubstitution([FindExecutable(name="xacro")]),
             " ",
-            PathJoinSubstitution(
-                [
-                    FindPackageShare("layered_hardware"),
-                    "examples/single_actuator/description",
-                    "robot_description.urdf.xacro",
-                ]
-            ),
+            robot_description_path,
         ]
     )
     robot_description = {"robot_description": ParameterValue(robot_description_content, value_type=str)}
 
+    # Get controller configs
     robot_controllers = PathJoinSubstitution(
         [
             FindPackageShare("layered_hardware"),
@@ -44,6 +56,8 @@ def generate_launch_description():
             "controllers.yaml",
         ]
     )
+
+    # Get display configs
     rviz_config_file = PathJoinSubstitution(
         [
             FindPackageShare("layered_hardware"), 
@@ -51,6 +65,7 @@ def generate_launch_description():
             "display_config.rviz"]
     )
 
+    # Declare control nodes
     control_node = Node(
         package="controller_manager",
         executable="ros2_control_node",
@@ -66,21 +81,12 @@ def generate_launch_description():
         output="both",
         parameters=[robot_description],
     )
-    rviz_node = Node(
-        package="rviz2",
-        executable="rviz2",
-        output="both",
-        arguments=["-d", rviz_config_file],
-        # condition=IfCondition(gui),
-    )
-
     controller_spawner = Node(
         package="controller_manager",
         executable="spawner",
         arguments=["joint_state_broadcaster", "position_controller",
                    "--controller-manager", "/controller_manager"],
     )
-
     sub_controller_spawner = Node(
         package="controller_manager",
         executable="spawner",
@@ -88,6 +94,16 @@ def generate_launch_description():
                    "--inactive", "--controller-manager", "/controller_manager"],
     )
 
+    # Declare visualization nodes
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        output="both",
+        arguments=["-d", rviz_config_file],
+        condition=IfCondition(gui),
+    )
+
+    # Build all nodes
     nodes = [
         control_node,
         robot_state_pub_node,
