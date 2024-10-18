dbb31b07f28cecef55ed2ef39772ab383076ae6d
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2024-10-18 05:49:36 +0000

    visualization of single actuator example

--- examples/single_actuator/launch/single_actuator_example.launch.py
+++ examples/single_actuator/launch/single_actuator_example.launch.py
@@ -1,7 +1,6 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, RegisterEventHandler
 from launch.conditions import IfCondition
-from launch.event_handlers import OnProcessExit
 from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
 
 from launch_ros.actions import Node
@@ -45,9 +44,12 @@ def generate_launch_description():
             "controllers.yaml",
         ]
     )
-    # rviz_config_file = PathJoinSubstitution(
-    #     [FindPackageShare("ros2_control_demo_description"), "rrbot/rviz", "rrbot.rviz"]
-    # )
+    rviz_config_file = PathJoinSubstitution(
+        [
+            FindPackageShare("layered_hardware"), 
+            "examples/single_actuator/config", 
+            "display_config.rviz"]
+    )
 
     control_node = Node(
         package="controller_manager",
@@ -64,14 +66,13 @@ def generate_launch_description():
         output="both",
         parameters=[robot_description],
     )
-    # rviz_node = Node(
-    #     package="rviz2",
-    #     executable="rviz2",
-    #     name="rviz2",
-    #     output="log",
-    #     arguments=["-d", rviz_config_file],
-    #     condition=IfCondition(gui),
-    # )
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        output="both",
+        arguments=["-d", rviz_config_file],
+        # condition=IfCondition(gui),
+    )
 
     controller_spawner = Node(
         package="controller_manager",
@@ -87,20 +88,12 @@ def generate_launch_description():
                    "--inactive", "--controller-manager", "/controller_manager"],
     )
 
-    # Delay rviz start after `joint_state_broadcaster`
-    # delay_rviz_after_joint_state_broadcaster_spawner = RegisterEventHandler(
-    #     event_handler=OnProcessExit(
-    #         target_action=joint_state_broadcaster_spawner,
-    #         on_exit=[rviz_node],
-    #     )
-    # )
-
     nodes = [
         control_node,
         robot_state_pub_node,
         controller_spawner,
         sub_controller_spawner,
-        # delay_rviz_after_joint_state_broadcaster_spawner,
+        rviz_node
     ]
 
     return LaunchDescription(declared_arguments + nodes)
