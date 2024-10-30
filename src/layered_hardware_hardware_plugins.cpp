49733939e099badbfe72286ebcff25161257ad6e
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2024-10-30 04:24:39 +0000

    separate plugins for different packages

---
+++ src/layered_hardware_hardware_plugins.cpp
@@ -0,0 +1,4 @@
+#include <layered_hardware/layered_hardware.hpp>
+#include <pluginlib/class_list_macros.hpp>
+
+PLUGINLIB_EXPORT_CLASS(layered_hardware::LayeredHardware, hardware_interface::SystemInterface);
--- src/layered_hardware_layer_plugins.cpp
+++ src/layered_hardware_layer_plugins.cpp
@@ -1,6 +1,5 @@
 #include <layered_hardware/joint_limits_layer.hpp>
 #include <layered_hardware/layer_interface.hpp>
-#include <layered_hardware/layered_hardware.hpp>
 #include <layered_hardware/mock_actuator_layer.hpp>
 #include <layered_hardware/monitor_layer.hpp>
 #include <layered_hardware/transmission_layer.hpp>
@@ -10,4 +9,3 @@ PLUGINLIB_EXPORT_CLASS(layered_hardware::JointLimitsLayer, layered_hardware::Lay
 PLUGINLIB_EXPORT_CLASS(layered_hardware::MockActuatorLayer, layered_hardware::LayerInterface);
 PLUGINLIB_EXPORT_CLASS(layered_hardware::MonitorLayer, layered_hardware::LayerInterface);
 PLUGINLIB_EXPORT_CLASS(layered_hardware::TransmissionLayer, layered_hardware::LayerInterface);
-PLUGINLIB_EXPORT_CLASS(layered_hardware::LayeredHardware, hardware_interface::SystemInterface);
